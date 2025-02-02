a3d27daa6cff0f010843db2992fc976aee003f8e
Arijit Debnath <itsarijitdebnath@gmail.com>
2025-02-02 19:21:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# codesetup
+
+`codesetup` is a CLI tool to quickly scaffold a new application with a customizable setup.
+
+## Features
+
+- Interactive prompts for creating a new Express project
+- Supports TypeScript templates.
+- Initializes a Git repository and installs dependencies
+
+## Installation
+
+Install `codesetup` globally using npm:
+
+\`\`\`bash
+npm install -g codesetup
+\`\`\`
+
+## Usage
+
+After installation, run the CLI command to create a new Node.js application:
+
+\`\`\`bash
+npx codesetup
+\`\`\`
+
+Follow the prompts to select your preferred template and options.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import chalk from "chalk";
+import fs from "fs-extra";
+import path from "path";
+import { simpleGit } from "simple-git";
+import ora from "ora";
+import inquirer from "inquirer";
+import { execSync } from "child_process";
+
+const program = new Command();
+
+program.version("1.0.0").action(async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What would you call your project?",
+      required: true,
+    },
+    {
+      type: "select",
+      name: "package-manager",
+      message: "Which package manager would you like to use?",
+      choices: [
+        {
+          name: "npm",
+          value: "npm",
+        },
+        {
+          name: "pnpm",
+          value: "pnpm",
+        },
+        {
+          name: "yarn",
+          value: "yarn",
+        },
+      ],
+      default: "npm",
+    },
+  ]);
+
+  const projectName = answers.name;
+  const projectPath = path.join(process.cwd(), projectName);
+  const packageManager = answers["package-manager"];
+
+  if (fs.exists(projectPath)) {
+    console.error(chalk.red(`Error: Directory ${projectName} already exists.`));
+    process.exit(1);
+  }
+
+  const repoUrl = "https://github.com/arijitdn/express-starter-template.git";
+
+  fs.mkdirsSync(projectPath);
+
+  const spinner = ora(`Setting up Express starter template...`).start();
+  const git = simpleGit();
+
+  try {
+    await git.clone(repoUrl, projectPath);
+    spinner.succeed("Express starter template is ready to use!");
+
+    process.chdir(projectPath);
+
+    spinner.start("Installing dependencies...");
+    execSync(`${packageManager} install`, { stdio: "inherit" });
+    spinner.succeed("Dependencies installed successfully!");
+
+    console.log(chalk.green(`${projectName} is ready to use!`));
+    console.log(
+      chalk.green(
+        `cd ${projectName} and run ${
+          packageManager === "npm" ? "npm run dev" : `${packageManager} dev`
+        }`
+      )
+    );
+  } catch (error) {
+    spinner.fail("Failed to setup the project.");
+    console.error(chalk.red(error.message));
+    process.exit(1);
+  }
+});
+
+program.parse(process.argv);
